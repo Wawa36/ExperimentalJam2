32fb3ac9f8175bc527162944193908d1e6978063
Jonathan Veit <Official.jonathanveit@gmail.com>
2020-06-12 17:24:15 +0200

    [Added] Singleton
    Added the Singleton Class

---
+++ Assets/01_Scripts/Singleton.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    static T instance;
+
+    public static T Instance
+    {
+        get 
+        {
+            if (instance)
+            {
+                return instance;
+            }
+            else
+            {
+                var t = GameObject.FindObjectOfType<T>();
+
+                if (t)
+                    return t;
+                else
+                    return new GameObject("Singleton: " + typeof(T).ToString()).AddComponent<T>();
+            }
+        }
+    }
+
+    public virtual void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this as T;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+}
