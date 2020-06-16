a2a47e2b3c34a82a91b10366e16e579ba885e8cd
Jonathan Veit <Official.jonathanveit@gmail.com>
2020-06-16 14:07:12 +0200

    [changed] tower counts steps

--- Assets/00_Scenes/Jonathan/Scripts/JonathanAlgorithmus.cs
+++ Assets/00_Scenes/Jonathan/Scripts/JonathanAlgorithmus.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Tower_Management;
+using System.Runtime.InteropServices;
 
 static class JonathanAlgorithmus
 {
@@ -18,14 +19,20 @@ static class JonathanAlgorithmus
         }
         else
         {
-            dir = new Vector3(Random.Range(0, 2) == 0 ? 1 : -1, Random.Range(0, 2) == 0 ? 1 : 0, Random.Range(0, 2) == 0 ? 1 : -1).normalized;
+            while (true)
+            {
+                dir = new Vector3(Random.Range(0, 2) == 0 ? 1 : -1, Random.Range(0, 2) == 0 ? 1 : 0, Random.Range(0, 2) == 0 ? 1 : -1).normalized;
+
+                if (Vector3.Dot(dir, at_building.Cambium.normal) >= 0)
+                    break;
+            }
         }
 
         var ray = new Ray(at_building.Main_Collider.transform.position + dir * 100f, -dir);
         var hit = new RaycastHit();
         at_building.Main_Collider.Raycast(ray, out hit, Mathf.Infinity);
 
-        kambiumList.Add(new Tower.Cambium(hit.point, hit.normal, tower.Building_Prefabs[Random.Range(0, tower.Building_Prefabs.Count)], at_building.Cambium.steps > 0 ? at_building.Cambium.steps - 1 : Random.Range(0, tower.Steps)));
+        kambiumList.Add(new Tower.Cambium(hit.point, hit.normal, tower.Building_Prefabs[Random.Range(0, tower.Building_Prefabs.Count)], at_building.Cambium.steps > 0 ? at_building.Cambium.steps : Random.Range(0, tower.Steps)));
 
         return new Tower.Cambiums_At_Active(at_building, kambiumList.ToArray());
     }
