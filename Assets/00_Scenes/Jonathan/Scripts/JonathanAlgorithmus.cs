0a7c1b0d9d68857429fc1f9b78d2e17e3bc6b3a2
Jonathan Veit <Official.jonathanveit@gmail.com>
2020-06-16 15:08:19 +0200

    [Added] Building 10

--- Assets/00_Scenes/Jonathan/Scripts/JonathanAlgorithmus.cs
+++ Assets/00_Scenes/Jonathan/Scripts/JonathanAlgorithmus.cs
@@ -26,10 +26,25 @@ static class JonathanAlgorithmus
         {
             while (true)
             {
-                dir = new Vector3(Random.Range(0, 2) == 0 ? 1 : -1, Random.Range(0, 2) == 0 ? 1 : 0, Random.Range(0, 2) == 0 ? 1 : -1).normalized;
+                // raycast with random dir
+                dir = new Vector3(Random.Range(0, 2) == 0 ? 1 : -1, Random.Range(0, 3) == 0 ? 1 : 0, Random.Range(0, 2) == 0 ? 1 : -1).normalized;
                 ray = new Ray(at_building.Main_Collider.transform.position + dir * 100f, -dir);
                 at_building.Main_Collider.Raycast(ray, out hit, Mathf.Infinity);
-                Debug.Log(Vector3.Dot(hit.normal, at_building.Cambium.normal));
+
+
+                // check distance to origin
+                Vector2 tower_2D = new Vector2(tower.transform.position.x, tower.transform.position.z);
+                Vector2 building_2D = new Vector2(at_building.Main_Collider.transform.position.x, at_building.Main_Collider.transform.position.z);
+                Debug.Log(Vector2.Distance(tower_2D, building_2D));
+
+                if (Vector2.Distance(tower_2D, building_2D) > 5)
+                {
+                    Debug.DrawRay(building_2D, tower_2D - building_2D, Color.red);
+                    dir = tower_2D - building_2D;
+                    break;
+                }
+
+                // break if normal goes to opposite dir of former ray 
                 if (Vector3.Dot(hit.normal, at_building.Cambium.normal) >= -0.5f)
                     break;
             }
