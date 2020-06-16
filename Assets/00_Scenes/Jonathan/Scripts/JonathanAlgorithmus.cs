aba5793850308c22d7a8d89367883f395033378d
Jonathan Veit <Official.jonathanveit@gmail.com>
2020-06-16 11:37:06 +0200

    [added] Script with new Jonathan Algorithmus

---
+++ Assets/00_Scenes/Jonathan/Scripts/JonathanAlgorithmus.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Tower_Management;
+
+public class JonathanAlgorithmus : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Tower.Cambium[] JonathansKabiumAlgo(Building at_building, Tower tower)
+    {
+        List<Tower.Cambium> kambiumList = new List<Tower.Cambium>();
+
+        // raycast to main collider
+        Vector3 dir;
+
+        if (at_building.Cambium.steps > 0)
+        {
+            dir = at_building.Cambium.normal;
+        }
+        else
+        {
+            dir = new Vector3(Random.Range(0, 2) == 0 ? 1 : -1, Random.Range(0, 2) == 0 ? 1 : 0, Random.Range(0, 2) == 0 ? 1 : -1).normalized;
+        }
+
+        var ray = new Ray(at_building.Main_Collider.transform.position + dir * 100f, -dir);
+        var hit = new RaycastHit();
+        at_building.Main_Collider.Raycast(ray, out hit, Mathf.Infinity);
+
+        kambiumList.Add(new Tower.Cambium(hit.point, hit.normal, tower.Building_Prefabs[Random.Range(0, tower.Building_Prefabs.Count)], at_building.Cambium.steps > 0? at_building.Cambium.steps - 1 : 0));
+
+        return kambiumList.ToArray();
+    }
+}
