2aa2fae27f5f175511d9eefdecad9596ab319cb9
Jonathan Veit <Official.jonathanveit@gmail.com>
2020-06-17 11:09:28 +0200

    [added] Tower prefabs and Playtesting Scene

--- Assets/00_Scenes/Jonathan/Scripts/JonathanAlgorithmus.cs
+++ Assets/00_Scenes/Jonathan/Scripts/JonathanAlgorithmus.cs
@@ -6,6 +6,7 @@ using System.Runtime.InteropServices;
 
 static class JonathanAlgorithmus
 {
+    // RBunker
     public static Tower.Cambiums_At_Active RBunker(Building at_building, Tower tower)
     {
         List<Tower.Cambium> kambiumList = new List<Tower.Cambium>();
@@ -57,10 +58,15 @@ static class JonathanAlgorithmus
         return new Tower.Cambiums_At_Active(at_building, kambiumList.ToArray());
     }
 
-    static int branch_amount = 1;
+    // RBunker Branch
+    static Dictionary<Tower, int> branches_per_tower = new Dictionary<Tower, int>();
 
     public static Tower.Cambiums_At_Active RBunkerBranch(Building at_building, Tower tower)
     {
+        // add tower to dic
+        if (!branches_per_tower.ContainsKey(tower))
+            branches_per_tower.Add(tower, 1);
+
         // cambiums
         List<Tower.Cambium> kambiumList = new List<Tower.Cambium>();
 
@@ -90,7 +96,7 @@ static class JonathanAlgorithmus
                 Vector2 tower_2D = new Vector2(tower.transform.position.x, tower.transform.position.z);
                 Vector2 building_2D = new Vector2(at_building.Main_Collider.transform.position.x, at_building.Main_Collider.transform.position.z);
 
-                if (Vector2.Distance(tower_2D, building_2D) > 30)
+                if (Vector2.Distance(tower_2D, building_2D) > 60)
                 {   
                     dir = tower.transform.position - at_building.transform.position;
                     dir.y = 0;
@@ -109,15 +115,15 @@ static class JonathanAlgorithmus
         kambiumList.Add(new Tower.Cambium(hit.point, hit.normal, tower.Building_Prefabs[Random.Range(0, tower.Building_Prefabs.Count)], at_building.Cambium.steps > 0 ? at_building.Cambium.steps : Random.Range(0, tower.Steps)));
 
         // delet random cambium
-        if (Random.Range(0, 50) == 0 && branch_amount > 1)
+        if (Random.Range(0, 50) == 0 && branches_per_tower[tower] > 1)
         {
             kambiumList.RemoveAt(0);
 
-            branch_amount--;
+            branches_per_tower[tower]--;
         }
 
         // new random cambium
-        if (Random.Range(0, 50) == 0 && branch_amount < 4)
+        if (Random.Range(0, 50) == 0 && branches_per_tower[tower] < 4)
         {
             // raycast with random dir
             dir = new Vector3(Random.Range(0, 2) == 0 ? 1 : -1, Random.Range(0, 5) == 0 ? 1 : 0, Random.Range(0, 2) == 0 ? 1 : -1).normalized;
@@ -125,7 +131,7 @@ static class JonathanAlgorithmus
             at_building.Main_Collider.Raycast(ray, out hit, Mathf.Infinity);
             kambiumList.Add(new Tower.Cambium(hit.point, hit.normal, tower.Building_Prefabs[Random.Range(0, tower.Building_Prefabs.Count)], Random.Range(0, tower.Steps)));
 
-            branch_amount++;
+            branches_per_tower[tower]++;
         }
         
 
