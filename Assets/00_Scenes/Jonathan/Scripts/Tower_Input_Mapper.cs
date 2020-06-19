2c7cec1704c7a7cd46a702fedb7397b237e4ad20
Jonathan Veit <Official.jonathanveit@gmail.com>
2020-06-19 14:20:55 +0200

    [added] Tower Mapping Selection

--- Assets/00_Scenes/Jonathan/Scripts/Tower_Input_Mapper.cs
+++ Assets/00_Scenes/Jonathan/Scripts/Tower_Input_Mapper.cs
@@ -7,6 +7,11 @@ namespace Tower_Management
 {
     public class Tower_Input_Mapper : MonoBehaviour
     {
+        [SerializeField] Property grow_speed;
+        [SerializeField] Property split_chance;
+        [SerializeField] Property generation_amount;
+        [SerializeField] Property change_direction_chance;
+
         Tower.Player_Inputs inputs;
 
         public void Initialize(Tower.Player_Inputs inputs)
@@ -17,7 +22,7 @@ namespace Tower_Management
         // properties 
         public float Grow_Speed { get { return Calculate_Grow_Speed(); } }
 
-        public Vector3 Grow_Direction { get { return Calculate_Grow_Direction(); } }
+        public Vector3 Grow_Direction { get { return inputs.player_dir; } }
 
         public int Split_Chance { get { return Calculate_Split_Chance(); } }
 
@@ -25,31 +30,93 @@ namespace Tower_Management
 
         public int Change_Direction_Chance { get { return Calculate_Change_Direction_Chance(); } }
 
+        public string Ground_Tag { get { return inputs.ground_tag; } }
+
         // calculations
 
         float Calculate_Grow_Speed()
         {
-            return 1;
-        }
-
-        Vector3 Calculate_Grow_Direction()
-        {
-            return inputs.player_dir;
+            switch (grow_speed.input)
+            {
+                case input_values.OrbEnergy:
+                    return  Map_Value(inputs.orb_energy, grow_speed.min, grow_speed.max);
+                case input_values.PlayerSpeed:
+                    return Map_Value(inputs.player_speed, grow_speed.min, grow_speed.max);
+                case input_values.ThrowDistance:
+                    return Map_Value(inputs.orb_energy, grow_speed.min, grow_speed.max);
+                case input_values.ThrowTime:
+                    return Map_Value(inputs.player_speed, grow_speed.min, grow_speed.max);
+                default:
+                    return default;
+            }
         }
 
         int Calculate_Split_Chance()
         {
-            return default;
+            switch (split_chance.input)
+            {
+                case input_values.OrbEnergy:
+                    return Map_Value(inputs.orb_energy, grow_speed.min, grow_speed.max);
+                case input_values.PlayerSpeed:
+                    return Map_Value(inputs.player_speed, grow_speed.min, grow_speed.max);
+                case input_values.ThrowDistance:
+                    return Map_Value(inputs.orb_energy, grow_speed.min, grow_speed.max);
+                case input_values.ThrowTime:
+                    return Map_Value(inputs.player_speed, grow_speed.min, grow_speed.max);
+                default:
+                    return default;
+            }
         }
 
         int Calculate_Generation_Amount()
         {
-            return 50;
+            switch (generation_amount.input)
+            {
+                case input_values.OrbEnergy:
+                    return Map_Value(inputs.orb_energy, grow_speed.min, grow_speed.max);
+                case input_values.PlayerSpeed:
+                    return Map_Value(inputs.player_speed, grow_speed.min, grow_speed.max);
+                case input_values.ThrowDistance:
+                    return Map_Value(inputs.orb_energy, grow_speed.min, grow_speed.max);
+                case input_values.ThrowTime:
+                    return Map_Value(inputs.player_speed, grow_speed.min, grow_speed.max);
+                default:
+                    return default;
+            }
         }
 
         int Calculate_Change_Direction_Chance() 
         {
-            return default;
-        } 
+            switch (change_direction_chance.input)
+            {
+                case input_values.OrbEnergy:
+                    return Map_Value(inputs.orb_energy, grow_speed.min, grow_speed.max);
+                case input_values.PlayerSpeed:
+                    return Map_Value(inputs.player_speed, grow_speed.min, grow_speed.max);
+                case input_values.ThrowDistance:
+                    return Map_Value(inputs.orb_energy, grow_speed.min, grow_speed.max);
+                case input_values.ThrowTime:
+                    return Map_Value(inputs.player_speed, grow_speed.min, grow_speed.max);
+                default:
+                    return default;
+            }
+        }
+
+        // mapping 
+        int Map_Value (float value, int min, int max) 
+        {
+            return (int) Mathf.InverseLerp(min, max, value);
+        }
+
+        enum input_values {OrbEnergy, ThrowDistance, ThrowTime, PlayerSpeed };
+
+        [System.Serializable]
+        struct Property
+        {
+            public input_values input;
+            public int min;
+            public int max;
+            public int multiplier;
+        }
     }
 }
