1e20a2cd59fd6edfbb4f60f21c327aecb2d1406c
Jonathan Veit <Official.jonathanveit@gmail.com>
2020-06-23 15:10:40 +0200

    [added] UI Prefab

---
+++ Assets/00_Scenes/Jonathan/Scripts/FPS.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FPS : MonoBehaviour
+{
+    [SerializeField] Text display;
+    [SerializeField] [Range(0, 5)] int rate;
+
+    float counter = 100;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (counter >= rate)
+        {
+            display.text = Mathf.Round(1 / Time.deltaTime).ToString() + " fps";
+            counter = 0;
+        }
+        else
+            counter+= Time.deltaTime;
+    }
+}
