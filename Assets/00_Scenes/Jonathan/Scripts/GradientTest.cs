e82cf9fd2436b1239443512b41fb099485ef5915
Jonathan Veit <Official.jonathanveit@gmail.com>
2020-06-26 12:30:03 +0200

    [added] nothing

---
+++ Assets/00_Scenes/Jonathan/Scripts/GradientTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GradientTest : MonoBehaviour
+{
+    public LineRenderer grad;
+    public Color col;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var c = grad.colorGradient.colorKeys;
+        c[0].color = col;
+        grad.colorGradient.colorKeys = c;
+        grad.startColor = col;
+    }
+}
