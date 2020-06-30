18ff309d878a2608edd0809453ecb4ef250c287a
Jonathan Veit <Official.jonathanveit@gmail.com>
2020-06-30 12:18:59 +0200

    [added] clouds

---
+++ Assets/00_Scenes/Jonathan/Scripts/Player_Animation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Animation : MonoBehaviour
+{
+    [SerializeField] PlayerMovement player;
+    [SerializeField] Animator anim;
+    [SerializeField] float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var vel = player.velocity;
+        vel.y = 0;
+        anim.SetFloat("Speed", vel.magnitude * speed);    
+    }
+}
