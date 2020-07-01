2f139d19301468010eed1d22d8dfb7adc80e8a9e
Jonathan Veit <Official.jonathanveit@gmail.com>
2020-07-01 19:25:22 +0200

    [canged] orb anim speed

--- Assets/00_Scenes/Jonathan/Scripts/World_End.cs
+++ Assets/00_Scenes/Jonathan/Scripts/World_End.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Build.Player;
 using UnityEngine;
 
 public class World_End : MonoBehaviour
 {
-    private void OnCollisionEnter(Collision collision)
+    [SerializeField] float at_y_position;
+
+    private void Update()
     {
-      
+        if (PlayerMovement.Instance.transform.position.y < at_y_position)
+            StartCoroutine(PlayerMovement.Instance.Teleporting(PlayerMovement.Instance.transform.position, PlayerMovement.Instance.LastGroundedPlace));
     }
 }
