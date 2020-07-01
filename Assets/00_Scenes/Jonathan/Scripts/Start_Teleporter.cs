b742569f4b64e46706e19276186cb270e898cee5
Jonathan Veit <Official.jonathanveit@gmail.com>
2020-07-01 14:43:11 +0200

    [nothing]

--- Assets/00_Scenes/Jonathan/Scripts/Start_Teleporter.cs
+++ Assets/00_Scenes/Jonathan/Scripts/Start_Teleporter.cs
@@ -6,11 +6,12 @@ using UnityEngine;
 public class Start_Teleporter : MonoBehaviour
 {
     [SerializeField] Vector3 target_position;
+    [SerializeField] float speed;
 
     // Start is called before the first frame update
     void Start()
     {
-        PlayerMovement.Instance.StartCoroutine(PlayerMovement.Instance.Teleporting(PlayerMovement.Instance.transform.position, target_position));
+        PlayerMovement.Instance.StartCoroutine(PlayerMovement.Instance.Teleporting(PlayerMovement.Instance.transform.position, target_position, speed));
         Destroy(this);
     }
 }
