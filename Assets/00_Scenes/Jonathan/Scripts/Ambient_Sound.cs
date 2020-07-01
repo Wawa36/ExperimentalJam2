29d76778bb3ef4102812a119ae71957d9584ba41
Jonathan Veit <Official.jonathanveit@gmail.com>
2020-07-01 22:07:10 +0200

    [added] Grow Color Lerp

---
+++ Assets/00_Scenes/Jonathan/Scripts/Ambient_Sound.cs
@@ -0,0 +1,30 @@
+﻿using Settings_Management;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ambient_Sound : MonoBehaviour
+{
+    [SerializeField] float start_height;
+    [SerializeField] float max_height;
+    [SerializeField] AudioSource source;
+    public float value;
+    Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        source.clip = Sound_Manager.Instance.Get_Clip("Wind Atmo").clip;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float player_y_pos = player.position.y;
+
+        float volume = Mathf.InverseLerp(start_height, max_height, player_y_pos);
+        value = Mathf.InverseLerp(start_height, max_height, player_y_pos);
+        source.volume = volume * Sound_Manager.Instance.Get_Clip("Wind Atmo").volume;
+    }
+}
